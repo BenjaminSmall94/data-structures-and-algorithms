9a1e71088677fa83a65aa65dc907b95be5082909
Benjamin Small <smallbenjamin94@gmail.com>
2022-07-22 17:37:45 -0700

    initial commit

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -3,14 +3,33 @@ class LinkedList:
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, head=None):
+        self.head = head
 
-    def some_method(self):
-        # method body here
-        pass
+    def insert(self, value):
+        self.head = Node(value, self.head)
 
+    def includes(self, value):
+        curr = self.head
+        while curr is not None:
+            if curr.value == value:
+                return True
+            curr = curr.next
+
+    def __str__(self):
+        curr = self.head
+        output = ""
+        while curr is not None:
+            output += "{ " + str(curr) + "} -> "
+        output += "NULL"
+
+class Node:
+    """
+
+    """
+    def __init__(self, value, next_node):
+        self.value = value
+        self.next = next_node
 
 class TargetError:
     pass
