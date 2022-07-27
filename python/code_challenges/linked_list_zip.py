74610894ae61263e16380f2df6a407e8143d858f
Benjamin Small <smallbenjamin94@gmail.com>
2022-07-27 14:47:01 -0700

    completed Code Challenge 8 according to assignment specifications

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,2 +1,21 @@
+from data_structures.linked_list import LinkedList
+
+
 def zip_lists(a, b):
-    pass
+    curr_a = a.head
+    curr_b = b.head
+    new_linked_list = LinkedList()
+    while curr_a is not None and curr_b is not None:
+        print(curr_a.value)
+        print(curr_b.value)
+        new_linked_list.append(curr_a.value)
+        new_linked_list.append(curr_b.value)
+        curr_a = curr_a.next
+        curr_b = curr_b.next
+    while curr_a is not None:
+        new_linked_list.append(curr_a.value)
+        curr_a = curr_a.next
+    while curr_b is not None:
+        new_linked_list.append(curr_b.value)
+        curr_b = curr_b.next
+    return new_linked_list
