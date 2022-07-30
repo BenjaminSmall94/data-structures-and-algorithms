789290f3c384be81bd2e2293348e15a2cdcb6a83
Benjamin Small <smallbenjamin94@gmail.com>
2022-07-29 23:54:12 -0700

    completed Code Challenge 10 according to assignment specifications

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,42 @@
+from data_structures.linked_list import Node
+from data_structures.invalid_operation_error import InvalidOperationError
+
 class Queue:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
+        self.front = None
+        self.rear = None
         pass
 
-    def some_method(self):
-        # method body here
+    def enqueue(self, value):
+        new_node = Node(value)
+        if(self.is_empty()):
+            self.front = new_node
+            self.rear = new_node
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+
+    def dequeue(self):
+        if not self.is_empty():
+            popped_node = self.front
+            if self.front is self.rear:
+                self.rear = None
+            self.front = self.front.next
+            popped_node.next = None
+            return popped_node.value
+        else:
+            raise InvalidOperationError
+
+    def peek(self):
+        if self.is_empty():
+            raise InvalidOperationError("Method not allowed on empty collection")
+        else:
+            return self.front.value
+
+    def is_empty(self):
+        return self.front is None
         pass
