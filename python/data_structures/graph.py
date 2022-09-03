d6e64d894d54667e3f0a447a06eeab0d3165f524
Benjamin Small <smallbenjamin94@gmail.com>
2022-09-02 21:43:16 -0700

    completed code challenge 35 according to assignment specifications

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -4,13 +4,38 @@ class Graph:
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.num = 0
+        self.adjacency_list = {}
 
-    def some_method(self):
-        # method body here
-        pass
+    def add_node(self, value):
+        self.num += 1
+        new_vertex = Vertex(value)
+        self.adjacency_list[new_vertex] = []
+        return new_vertex
+
+    def add_edge(self, start_node, end_node, weight=None):
+        if start_node in self.adjacency_list and end_node in self.adjacency_list:
+            self.adjacency_list[start_node].append(Edge(end_node, weight))
+        else:
+            raise KeyError
+
+    def get_neighbors(self, start_node):
+        return self.adjacency_list[start_node]
+
+    def get_nodes(self):
+        return self.adjacency_list.keys()
+
+    def size(self):
+        return self.num
 
 
 class Vertex:
-    pass
+
+    def __init__(self, value):
+        self.value = value
+
+
+class Edge:
+    def __init__(self, vertex, weight=None):
+        self.vertex = vertex
+        self.weight = weight
